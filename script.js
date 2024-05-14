81fcd75c99eaa1472d586802276c1ea749f74bd4
Harsh vardhan rai <harshst0@gmail.com>
2024-05-14 14:42:27 +0530

    complete

---
+++ script.js
@@ -0,0 +1,17 @@
+var random1= Math.random();
+var n=Math.floor(random1*6+1);
+var haha= "dice" + n + ".png";
+document.querySelector(".img2").setAttribute("src",haha);
+
+
+
+
+var random2= Math.random();
+var m=Math.floor(random2*6+1);
+var hehe= "dice" + m + ".png";
+document.querySelector(".img1").setAttribute("src",hehe);
+
+
+
+
+
